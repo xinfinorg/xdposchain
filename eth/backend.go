fcd45b5c58357ed1aa0dc978588c3c3a75c6a795
Yogesh Agarwal <31632267+Yogesh188@users.noreply.github.com>
2019-01-27 15:05:57 +0530

    Refine comments for feature add reward to validators and signers block

--- eth/backend.go
+++ eth/backend.go
@@ -193,6 +193,8 @@ func New(ctx *node.ServiceContext, config *Config) (*Ethereum, error) {
 				Sign   uint64  `json:"sign"`
 				Reward float64 `json:"reward"`
 			}
+			number := header.Number.Uint64()
+			rCheckpoint := chain.Config().Clique.RewardCheckpoint
 
 			// Call to smart contract signer.
 			config := ctx.GetConfig()
@@ -207,11 +209,9 @@ func New(ctx *node.ServiceContext, config *Config) (*Ethereum, error) {
 			}
 			opts := new(bind.CallOpts)
 
-			number := header.Number.Uint64()
-			rCheckpoint := chain.Config().Clique.RewardCheckpoint
 			prevCheckpoint := number - rCheckpoint
 
-			if number > 0 && prevCheckpoint > 0 && number%rCheckpoint == 0 {
+			if number > 0 && prevCheckpoint > 0 {
 				// Not reward for singer of genesis block and only calculate reward at checkpoint block.
 				startBlockNumber := number - (rCheckpoint * 2) + 1
 				endBlockNumber := startBlockNumber + rCheckpoint - 2
