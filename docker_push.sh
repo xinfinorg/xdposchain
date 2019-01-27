0cdbd07170751a1432b0111d17b267fd491a0707
Punit Agarwal <punit2403@gmail.com>
2019-01-27 16:02:10 +0530

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+docker tag etiennenapoleone/tomochain etiennenapoleone/tomochain:latest
+docker tag etiennenapoleone/tomochain etiennenapoleone/tomochain:$(git log --pretty=format:'%h' -n 1 | cat)
 docker push etiennenapoleone/xdcchain:latest
 docker push etiennenapoleone/xdcchain:$(git log --pretty=format:'%h' -n 1 | cat)
