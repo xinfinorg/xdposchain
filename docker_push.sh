d57666e8b188def8c03d5aae608259f4b61b39a5
Punit Agarwal <punit2403@gmail.com>
2019-01-27 16:00:36 +0530

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker push etiennenapoleone/xdcchain:latest
 docker push etiennenapoleone/xdcchain:$(git log --pretty=format:'%h' -n 1 | cat)
