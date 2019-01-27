e5dfd6df72a8ca8cbfdd458b4b45cadea6b1a648
AniketJain <cool.aj98@gmail.com>
2019-01-27 12:10:43 +0530

    update blockchain.go

--- core/blockchain.go
+++ core/blockchain.go
@@ -30,7 +30,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/mclock"
 	"github.com/ethereum/go-ethereum/consensus"
-	"github.com/ethereum/go-ethereum/consensus/clique"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
