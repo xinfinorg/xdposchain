d147d998d8d8f32de55d0f331ea700f3bff0f8fd
Punit Agarwal <punit2403@gmail.com>
2019-01-29 11:58:18 +0530

    Update ValidatorKyc.sol

--- contracts/validator/contract/ValidatorKyc.sol
+++ contracts/validator/contract/ValidatorKyc.sol
@@ -3,49 +3,7 @@ pragma solidity ^0.4.21;
 // This contract is under development.
 // Refer to readme for further details.
 
-library SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
-    }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
-
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
-  }
-
-  /**
-  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-}
-
-contract TomoValidator {
+contract XDCValidator {
     using SafeMath for uint256;
 
     event Vote(address _voter, address _candidate, uint256 _cap);
@@ -148,7 +106,7 @@ contract TomoValidator {
         _;
     }
 
-    function TomoValidator (
+    function XDCValidator (
         address[] _candidates,
         uint256[] _caps,
         address _firstOwner,
