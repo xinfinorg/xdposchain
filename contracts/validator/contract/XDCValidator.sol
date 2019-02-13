4cf034883be2fc8ef03b066c4e000f0b170b6310
AnilChinchawale <Anil24593@gmail.com>
2019-02-13 16:13:19 +0530

    UploadKYC function fixed.

--- contracts/validator/contract/XDCValidator.sol
+++ contracts/validator/contract/XDCValidator.sol
@@ -3,7 +3,49 @@ pragma solidity ^0.4.21;
 // This contract is under development.
 // Refer to readme for further details.
 
-import "./libs/SafeMath.sol";
+
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
 
 
 contract XDCValidator {
@@ -137,9 +179,9 @@ contract XDCValidator {
 
 
     // uploadKYC : anyone can upload a KYC; its not equivalent to becoming an owner.
-    function uploadKYC(string test) external {
+    function uploadKYC(string kychash) external {
         require(bytes(KYCString[msg.sender]).length==0);
-        KYCString[msg.sender]=test;
+        KYCString[msg.sender]=kychash;
     }
 
     // propose : any non-candidate who has uploaded its KYC can become an owner by proposing a candidate.
