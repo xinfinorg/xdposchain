7c0c05dd7d5b724bd5d24a75a307492eda9fe89b
Sinh Vu <sinh.vu@sotatek.com>
2019-08-01 16:48:54 +0700

    comment main function in gc

--- cmd/gc/main.go
+++ cmd/gc/main.go
@@ -8,19 +8,11 @@ import (
 	"runtime"
 	"sync"
 	"sync/atomic"
-	"time"
 
-	"github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/eth"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/ethereum/go-ethereum/trie"
-	"github.com/hashicorp/golang-lru"
+	lru "github.com/hashicorp/golang-lru"
 	"github.com/syndtr/goleveldb/leveldb"
-	"github.com/syndtr/goleveldb/leveldb/util"
 )
 
 var (
@@ -51,68 +43,68 @@ type ResultProcessNode struct {
 }
 
 func main() {
-	flag.Parse()
-	lddb, _ := ethdb.NewLDBDatabase(*dir, eth.DefaultConfig.DatabaseCache, utils.MakeDatabaseHandles())
-	head := core.GetHeadBlockHash(lddb)
-	currentHeader := core.GetHeader(lddb, head, core.GetBlockNumber(lddb, head))
-	tridb := trie.NewDatabase(lddb)
-	catchEventInterupt(lddb.LDB())
-	cache, _ = lru.New(*cacheSize)
-	go func() {
-		for i := uint64(1); i <= currentHeader.Number.Uint64(); i++ {
-			hash := core.GetCanonicalHash(lddb, i)
-			root := core.GetHeader(lddb, hash, i).Root
-			trieRoot, err := trie.NewSecure(root, tridb, 0)
-			if err != nil {
-				continue
-			}
-			if running {
-				stateRoots <- TrieRoot{trieRoot, i}
-			} else {
-				break
-			}
-		}
-		if running {
-			close(stateRoots)
-		}
-	}()
-	for trieRoot := range stateRoots {
-		atomic.StoreInt32(&finish, 1)
-		if running {
-			for _, address := range cleanAddress {
-				enc := trieRoot.trie.Get(address.Bytes())
-				var data state.Account
-				rlp.DecodeBytes(enc, &data)
-				fmt.Println(time.Now().Format(time.RFC3339), "Start clean state address ", address.Hex(), " at block ", trieRoot.number)
-				signerRoot, err := resolveHash(data.Root[:], lddb.LDB())
-				if err != nil {
-					fmt.Println(time.Now().Format(time.RFC3339), "Not found clean state address ", address.Hex(), " at block ", trieRoot.number)
-					continue
-				}
-				batch := new(leveldb.Batch)
-				count := 1
-				list := []*StateNode{{node: signerRoot}}
-				for len(list) > 0 {
-					newList, total := findNewNodes(list, lddb.LDB(), batch)
-					count = count + 17*len(newList)
-					list = removeNodesNil(newList, total)
-				}
-				fmt.Println(time.Now().Format(time.RFC3339), "Finish clean state address ", address.Hex(), " at block ", trieRoot.number, " keys ", count)
-				err = lddb.LDB().Write(batch, nil)
-				if err != nil {
-					fmt.Println(time.Now().Format(time.RFC3339), "Write batch leveldb error", err)
-					os.Exit(1)
-				}
-			}
-		} else {
-			break
-		}
-		atomic.StoreInt32(&finish, 0)
-	}
-	fmt.Println(time.Now(), "compact")
-	lddb.LDB().CompactRange(util.Range{})
-	lddb.Close()
-	fmt.Println(time.Now(), "end")
+	// flag.Parse()
+	// lddb, _ := ethdb.NewLDBDatabase(*dir, eth.DefaultConfig.DatabaseCache, utils.MakeDatabaseHandles())
+	// head := core.GetHeadBlockHash(lddb)
+	// currentHeader := core.GetHeader(lddb, head, core.GetBlockNumber(lddb, head))
+	// tridb := trie.NewDatabase(lddb)
+	// catchEventInterupt(lddb.LDB())
+	// cache, _ = lru.New(*cacheSize)
+	// go func() {
+	// 	for i := uint64(1); i <= currentHeader.Number.Uint64(); i++ {
+	// 		hash := core.GetCanonicalHash(lddb, i)
+	// 		root := core.GetHeader(lddb, hash, i).Root
+	// 		trieRoot, err := trie.NewSecure(root, tridb, 0)
+	// 		if err != nil {
+	// 			continue
+	// 		}
+	// 		if running {
+	// 			stateRoots <- TrieRoot{trieRoot, i}
+	// 		} else {
+	// 			break
+	// 		}
+	// 	}
+	// 	if running {
+	// 		close(stateRoots)
+	// 	}
+	// }()
+	// for trieRoot := range stateRoots {
+	// 	atomic.StoreInt32(&finish, 1)
+	// 	if running {
+	// 		for _, address := range cleanAddress {
+	// 			enc := trieRoot.trie.Get(address.Bytes())
+	// 			var data state.Account
+	// 			rlp.DecodeBytes(enc, &data)
+	// 			fmt.Println(time.Now().Format(time.RFC3339), "Start clean state address ", address.Hex(), " at block ", trieRoot.number)
+	// 			signerRoot, err := resolveHash(data.Root[:], lddb.LDB())
+	// 			if err != nil {
+	// 				fmt.Println(time.Now().Format(time.RFC3339), "Not found clean state address ", address.Hex(), " at block ", trieRoot.number)
+	// 				continue
+	// 			}
+	// 			batch := new(leveldb.Batch)
+	// 			count := 1
+	// 			list := []*StateNode{{node: signerRoot}}
+	// 			for len(list) > 0 {
+	// 				newList, total := findNewNodes(list, lddb.LDB(), batch)
+	// 				count = count + 17*len(newList)
+	// 				list = removeNodesNil(newList, total)
+	// 			}
+	// 			fmt.Println(time.Now().Format(time.RFC3339), "Finish clean state address ", address.Hex(), " at block ", trieRoot.number, " keys ", count)
+	// 			err = lddb.LDB().Write(batch, nil)
+	// 			if err != nil {
+	// 				fmt.Println(time.Now().Format(time.RFC3339), "Write batch leveldb error", err)
+	// 				os.Exit(1)
+	// 			}
+	// 		}
+	// 	} else {
+	// 		break
+	// 	}
+	// 	atomic.StoreInt32(&finish, 0)
+	// }
+	// fmt.Println(time.Now(), "compact")
+	// lddb.LDB().CompactRange(util.Range{})
+	// lddb.Close()
+	// fmt.Println(time.Now(), "end")
 }
 
 func removeNodesNil(list [][17]*StateNode, length int) []*StateNode {
@@ -151,7 +143,7 @@ func resolveHash(n trie.HashNode, db *leveldb.DB) (trie.Node, error) {
 	if err != nil || enc == nil {
 		return nil, &trie.MissingNodeError{}
 	}
-	return trie.MustDecodeNode(n, enc, 0), nil
+	return trie.MustDecodeNode(n, enc), nil
 }
 
 func getAllChilds(n StateNode, db *leveldb.DB) ([17]*StateNode, error) {
