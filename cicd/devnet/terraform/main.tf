3bb6d56e4b8c7d94ae3e23c1b30cade9dbc2563a
Liam Lai <liam.icheng.lai@gmail.com>
2023-05-01 21:33:25 +1000

    remove other regions config

--- cicd/devnet/terraform/main.tf
+++ cicd/devnet/terraform/main.tf
@@ -13,14 +13,6 @@ terraform {
 provider "aws" {
   region  = "us-east-1"
 }
-provider "aws" {
-  alias = "sa-east-1"
-  region  = "sa-east-1"
-}
-provider "aws" {
-  alias = "ap-northeast-1"
-  region  = "ap-northeast-1"
-}
 
 provider "aws" {
   alias = "us-east-2"
