696646be07012ad2907b8f404b8aafa941c93b72
Liam Lai <liam.lai@datameshgroup.com.au>
2023-10-28 19:04:14 +1100

    revert sync pr

--- params/config.go
+++ params/config.go
@@ -337,10 +337,6 @@ func (c *XDPoSConfig) String() string {
 }
 
 func (c *XDPoSConfig) BlockConsensusVersion(num *big.Int, extraByte []byte, extraCheck bool) string {
-	// https://github.com/XinFinOrg/XDPoSChain/pull/186
-	if extraCheck && (len(extraByte) == 0 || extraByte[0] != 2) {
-		return ConsensusEngineVersion1
-	}
 
 	if c.V2 != nil && c.V2.SwitchBlock != nil && num.Cmp(c.V2.SwitchBlock) > 0 {
 		return ConsensusEngineVersion2
@@ -362,7 +358,7 @@ func (v *V2) UpdateConfig(round uint64) {
 		}
 	}
 	// update to current config
-	log.Info("[updateV2Config] Update config", "index", index, "round", round, "SwitchRound", v.AllConfigs[index].SwitchRound)
+	log.Warn("[updateV2Config] Update config", "index", index, "round", round, "SwitchRound", v.AllConfigs[index].SwitchRound)
 	v.CurrentConfig = v.AllConfigs[index]
 }
 
