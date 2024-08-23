a523f7740d56819adf04900af67f61d6fbcb9832
wanwiset25 <wanwiset25@gmail.com>
2024-08-24 02:28:06 +0700

    Revert "eth, les: add sanity checks for unbounded block fields (#19573)"
    This reverts commit 8f4a2694371c9c4f6e58a8c5d4f4a25d55e12cf2.

--- eth/handler.go
+++ eth/handler.go
@@ -810,9 +810,6 @@ func (pm *ProtocolManager) handleMsg(p *peer) error {
 		if err := msg.Decode(&request); err != nil {
 			return errResp(ErrDecode, "%v: %v", msg, err)
 		}
-		if err := request.sanityCheck(); err != nil {
-			return err
-		}
 		request.Block.ReceivedAt = msg.ReceivedAt
 		request.Block.ReceivedFrom = p
 
