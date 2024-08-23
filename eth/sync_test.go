63a08e5dc235809dbb021a31bd9350040c5f8f66
wanwiset25 <wanwiset25@gmail.com>
2024-08-24 02:27:35 +0700

    Revert "add xdpos protocol test cases"
    This reverts commit 1b781530c6bcacd3749c0f5bd23fc54619dbcae5.

--- eth/sync_test.go
+++ eth/sync_test.go
@@ -29,7 +29,6 @@ import (
 func TestFastSyncDisabling63(t *testing.T) { testFastSyncDisabling(t, 63) }
 func TestFastSyncDisabling64(t *testing.T) { testFastSyncDisabling(t, 64) }
 func TestFastSyncDisabling65(t *testing.T) { testFastSyncDisabling(t, 65) }
-func TestFastSyncDisabling100(t *testing.T) { testFastSyncDisabling(t, 100) }
 
 // Tests that fast sync gets disabled as soon as a real block is successfully
 // imported into the blockchain.
