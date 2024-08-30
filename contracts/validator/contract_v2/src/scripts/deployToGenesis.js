31cccc5b760389e0d9a14ae12b3ab550aa75cbb2
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-08-30 12:13:14 +0400

    Update deployToGenesis.js

--- contracts/validator/contract_v2/src/scripts/deployToGenesis.js
+++ contracts/validator/contract_v2/src/scripts/deployToGenesis.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 
 const abi = XDCValidator["abi"];
 const bytecode = XDCValidator["bytecode"];
+const deployedBytecode = XDCValidator["deployedBytecode"];
 
 fs.writeFile("./abi", JSON.stringify(abi, null, 3), "utf8", function (error) {
   if (error) {
@@ -12,6 +13,13 @@ fs.writeFile("./abi", JSON.stringify(abi, null, 3), "utf8", function (error) {
   console.log("Write success ");
 });
 fs.writeFile("./bytecode", bytecode, "utf8", function (error) {
+  if (error) {
+    console.log(error);
+    return false;
+  }
+  console.log("Write success ");
+});
+fs.writeFile("./deployedBytecode", deployedBytecode, "utf8", function (error) {
   if (error) {
     console.log(error);
     return false;
