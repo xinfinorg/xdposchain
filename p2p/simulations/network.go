55f30e75529e7ea67ee4470f2808a6b5aab97a25
Daniel Liu <liudaniel@qq.com>
2024-10-25 21:30:54 +0800

    all: fix staticcheck warning ST1006: don't use generic name self
    The name of a method’s receiver should be a reflection of its identity;
    often a one or two letter abbreviation of its type suffices (such as
    “c” or “cl” for “Client”). Don’t use generic names such as “me”, “this”
    or “self”, identifiers typical of object-oriented languages that place
    more emphasis on methods as opposed to functions. The name need not be
    as descriptive as that of a method argument, as its role is obvious and
    serves no documentary purpose. It can be very short as it will appear
    on almost every line of every method of the type; familiarity admits
    brevity. Be consistent, too: if you call the receiver “c” in one method,
    don’t call it “cl” in another.

--- p2p/simulations/network.go
+++ p2p/simulations/network.go
@@ -79,6 +79,13 @@ func (net *Network) Events() *event.Feed {
 	return &net.events
 }
 
+// NewNode adds a new node to the network with a random ID
+func (net *Network) NewNode() (*Node, error) {
+	conf := adapters.RandomNodeConfig()
+	conf.Services = []string{net.DefaultService}
+	return net.NewNodeWithConfig(conf)
+}
+
 // NewNodeWithConfig adds a new node to the network with the given config,
 // returning an error if a node with the same ID or name already exists
 func (net *Network) NewNodeWithConfig(conf *adapters.NodeConfig) (*Node, error) {
