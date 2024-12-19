81d8559685fdc4b4c18ac3ec10b86b3db58bef49
liam.lai <liam.lai@us>
2024-12-19 01:05:28 -0800

    close channel

--- miner/worker.go
+++ miner/worker.go
@@ -278,12 +278,15 @@ func (w *worker) update() {
 	MinePeriodCh := w.engine.(*XDPoS.XDPoS).MinePeriodCh
 	defer close(MinePeriodCh)
 	NewRoundCh := w.engine.(*XDPoS.XDPoS).NewRoundCh
+	defer close(NewRoundCh)
 
 	timeout := time.NewTimer(time.Duration(minePeriod) * time.Second)
-	c := make(chan struct{})
+	defer timeout.Stop()
+	c := make(chan struct{}, 1)
+	defer close(c)
 	finish := make(chan struct{})
 	defer close(finish)
-	defer timeout.Stop()
+
 	go func() {
 		for {
 			// A real event arrived, process interesting content
