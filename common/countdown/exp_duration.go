64792634b79aa8c79d3bfd4af964b3d942f7e147
Wang Gerui <wgr523@gmail.com>
2024-12-24 01:14:03 +0800

    feat: refine exp duration

--- common/countdown/exp_duration.go
+++ common/countdown/exp_duration.go
@@ -2,34 +2,55 @@
 package countdown
 
 import (
+	"math"
 	"time"
 
 	"github.com/XinFinOrg/XDPoSChain/core/types"
+	"github.com/XinFinOrg/XDPoSChain/log"
 )
 
+const max_exponent_upperbound uint8 = 32
+
 type ExpTimeoutDuration struct {
-	duration time.Duration
-	exponent uint32
+	duration     time.Duration
+	base         float64
+	max_exponent uint8
 }
 
-func NewExpTimeoutDuration(duration time.Duration, exponent uint32) *ExpTimeoutDuration {
-	return &ExpTimeoutDuration{
-		duration: duration,
-		exponent: exponent,
+func NewExpTimeoutDuration(duration time.Duration, base float64, max_exponent uint8) *ExpTimeoutDuration {
+	d := &ExpTimeoutDuration{
+		duration:     duration,
+		base:         base,
+		max_exponent: max_exponent,
+	}
+	d.sanityCheck()
+	return d
+}
+
+func (d *ExpTimeoutDuration) sanityCheck() {
+	if d.max_exponent >= max_exponent_upperbound {
+		log.Error("max_exponent (e)= >= max_exponent_upperbound (e_upper)", "e", d.max_exponent, "e_upper", max_exponent_upperbound)
+		panic("max_exponent (e)= >= max_exponent_upperbound (e_upper)")
+	}
+	if math.Pow(d.base, float64(d.max_exponent)) >= float64(math.MaxUint32) {
+		log.Error("base^max_exponent (b^e) should be less than 2^32", "b", d.base, "e", d.max_exponent)
+		panic("base^max_exponent (b^e) should be less than 2^32")
 	}
 }
 
 // The inputs should be: [blockchain, currentRound, highestQuorumCert's round]
 func (d *ExpTimeoutDuration) GetTimeoutDuration(inputs ...interface{}) time.Duration {
-	power := uint32(1)
+	power := float64(1)
 	if len(inputs) >= 3 {
 		if currentRound, ok := inputs[1].(types.Round); ok {
 			if highestRound, ok := inputs[2].(types.Round); ok {
 				// below statement must be true, just to prevent negative result
 				if highestRound < currentRound {
-					for i := 0; i < int(currentRound-highestRound)-1; i++ {
-						power *= d.exponent
+					exp := uint8(currentRound-highestRound) - 1
+					if exp > d.max_exponent {
+						exp = d.max_exponent
 					}
+					power = math.Pow(d.base, float64(exp))
 				}
 			}
 		}
@@ -44,8 +65,14 @@ func (d *ExpTimeoutDuration) SetParams(inputs ...interface{}) {
 		}
 	}
 	if len(inputs) >= 2 {
-		if exponent, ok := inputs[1].(uint32); ok {
-			d.exponent = exponent
+		if base, ok := inputs[1].(float64); ok {
+			d.base = base
 		}
 	}
+	if len(inputs) >= 3 {
+		if exponent, ok := inputs[2].(uint8); ok {
+			d.max_exponent = exponent
+		}
+	}
+	d.sanityCheck()
 }
--- common/countdown/exp_duration_test.go
+++ common/countdown/exp_duration_test.go
@@ -9,37 +9,61 @@ import (
 )
 
 func TestExpDuration(t *testing.T) {
-	exponent := uint32(2)
-	base := time.Second * 59
-	duration := NewExpTimeoutDuration(base, exponent)
+	base := float64(2.0)
+	max_exponent := uint8(2)
+	duration := time.Second * 59
+	helper := NewExpTimeoutDuration(duration, base, max_exponent)
 	// round 10 = 9+1, normal case, should be base
 	currentRound := types.Round(10)
 	highestQCRound := types.Round(9)
-	result := duration.GetTimeoutDuration(nil, currentRound, highestQCRound)
-	assert.Equal(t, base, result)
+	result := helper.GetTimeoutDuration(nil, currentRound, highestQCRound)
+	assert.Equal(t, duration, result)
 
 	// round 11 = 9+2, already 1 round timeout, should be base*exponent
 	currentRound++
-	result = duration.GetTimeoutDuration(nil, currentRound, highestQCRound)
-	assert.Equal(t, base*time.Duration(exponent), result)
+	result = helper.GetTimeoutDuration(nil, currentRound, highestQCRound)
+	assert.Equal(t, duration*time.Duration(base), result)
 
 	// round 12 = 9+3, already 2 rounds timeout, should be base*exponent^2
 	currentRound++
-	result = duration.GetTimeoutDuration(nil, currentRound, highestQCRound)
-	assert.Equal(t, base*time.Duration(exponent)*time.Duration(exponent), result)
+	result = helper.GetTimeoutDuration(nil, currentRound, highestQCRound)
+	assert.Equal(t, duration*time.Duration(base)*time.Duration(base), result)
 
 	// test compatible with const timeout user calling it
-	result = duration.GetTimeoutDuration(nil)
-	assert.Equal(t, base, result)
+	result = helper.GetTimeoutDuration(nil)
+	assert.Equal(t, duration, result)
 
 	// test SetParams
+	duration++
+	max_exponent++
 	base++
-	exponent++
-	duration.SetParams(base, exponent)
-	result = duration.GetTimeoutDuration(nil, currentRound, highestQCRound)
-	assert.Equal(t, base*time.Duration(exponent)*time.Duration(exponent), result)
-	base++
-	duration.SetParams(base)
-	result = duration.GetTimeoutDuration(nil, currentRound, highestQCRound)
-	assert.Equal(t, base*time.Duration(exponent)*time.Duration(exponent), result)
+	helper.SetParams(duration, base, max_exponent)
+	result = helper.GetTimeoutDuration(nil, currentRound, highestQCRound)
+	assert.Equal(t, duration*time.Duration(base)*time.Duration(base), result)
+	duration++
+	helper.SetParams(duration)
+	result = helper.GetTimeoutDuration(nil, currentRound, highestQCRound)
+	assert.Equal(t, duration*time.Duration(base)*time.Duration(base), result)
+
+	// round 14 = 9+5, already 4 rounds timeout, but max_exponent=3, should be base*exponent^3
+	currentRound++
+	currentRound++
+	result = helper.GetTimeoutDuration(nil, currentRound, highestQCRound)
+	assert.Equal(t, duration*time.Duration(base)*time.Duration(base)*time.Duration(base), result)
+}
+
+func TestInvalidParameter(t *testing.T) {
+	assert.Panics(t, func() {
+		base := float64(2.0)
+		max_exponent := uint8(32)
+		duration := time.Second * 59
+		_ = NewExpTimeoutDuration(duration, base, max_exponent)
+	})
+
+	assert.Panics(t, func() {
+		base := float64(3.0)
+		max_exponent := uint8(21)
+		duration := time.Second * 59
+		_ = NewExpTimeoutDuration(duration, base, max_exponent)
+	})
 }
